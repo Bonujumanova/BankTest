899a0d6c1c421d50dfb738448411198bd5edb27e
Bonu <bonujumanova@gmail.com>
2026-09-02 16:26:15 +0700

    Delete src/main/api directory

--- src/main/api/models/create_account_response.py
+++
@@ -1,7 +0,0 @@
-from src.main.api.models.base_model import BaseModel
-
-class CreateAccountResponse(BaseModel):
-    id: int
-    number: str
-    balance: float
-    
--- src/main/api/models/create_user_request.py
+++
@@ -1,6 +0,0 @@
-from src.main.api.models.base_model import BaseModel
-
-class CreateUserRequest(BaseModel):
-    username: str
-    password: str
-    role: str
